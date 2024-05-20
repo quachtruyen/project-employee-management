81384cd50f174c02cb3c03460a12d683e2fbfd52
QuachTruyen <quachtruyen13@gmail.com>
2024-05-20 23:09:42 +0700

    add bootstrap

--- employee_manager/department/views.py
+++ employee_manager/department/views.py
@@ -5,8 +5,8 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 def get_department(request):
     department_list = department_model.objects.filter().order_by('department_id')
-    return render(request, 'department.html', {'department_list' : department_list})
+    return render(request, 'department/department.html', {'department_list' : department_list})
 
 def get_department_by_id(request, department_id):
     department = get_object_or_404(department_model, department_id=department_id)
-    return render(request, 'department_detail.html', {'department': department})
+    return render(request, 'department/department_detail.html', {'department': department})
--- employee_manager/employee_manager/settings.py
+++ employee_manager/employee_manager/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home',
     'employee',
     'department',
     'position',
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'employee_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
