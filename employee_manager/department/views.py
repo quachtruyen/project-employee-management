c02c8560d247c0069f2ed0abbbfca90a691e8762
QuachTruyen <quachtruyen13@gmail.com>
2024-05-21 10:59:12 +0700

    test commit

--- employee_manager/department/views.py
+++ employee_manager/department/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import department as department_model
+from employee.models import employee as employee_model
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -7,6 +8,7 @@ def get_department(request):
     department_list = department_model.objects.filter().order_by('department_id')
     return render(request, 'department/department.html', {'department_list' : department_list})
 
-def get_department_by_id(request, department_id):
-    department = get_object_or_404(department_model, department_id=department_id)
-    return render(request, 'department/department_detail.html', {'department': department})
+def get_department_by_id(request, dep_id):
+    employee_list = employee_model.objects.filter(department_id = dep_id).order_by('emp_id')
+    department = department_model.objects.get(department_id = dep_id)
+    return render(request, 'department/department_detail.html', {'employee_list': employee_list,'department': department})
